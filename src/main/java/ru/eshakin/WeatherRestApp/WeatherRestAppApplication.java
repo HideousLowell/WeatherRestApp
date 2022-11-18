e13fe4b51895089b91d884cc360449986dc3fa09
hlowell <email>
2022-11-18 16:51:54 +0700

    Added config

--- src/main/java/ru/eshakin/WeatherRestApp/WeatherRestAppApplication.java
+++ src/main/java/ru/eshakin/WeatherRestApp/WeatherRestAppApplication.java
@@ -1,19 +1,11 @@
 package ru.eshakin.WeatherRestApp;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WeatherRestAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WeatherRestAppApplication.class, args);
-    }
-
-    @Bean
-    public ModelMapper mapper() {
-        return new ModelMapper();
     }
 }
